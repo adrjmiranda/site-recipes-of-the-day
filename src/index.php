4b4d0bbb9e89723e80fc791991bc222a724fffa5
Adriano Miranda <adrjmiranda@gmail.com>
2023-06-26 16:55:40 -0300

    chore: add initial styles

--- src/index.php
+++ src/index.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="assets/recipesoftheday_icon.svg" type="image/x-icon">
   <title>Recipes Of The Day</title>
+  <!-- style sheet -->
+  <link rel="stylesheet" href="css/styles.css">
   <!-- google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,6 +15,16 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
-
+  <div class="container-expanded">
+    <div class="container">
+      <header>
+        <nav id="navbar">
+          <div class="logo">
+            <a href="#"><span class="material-symbols-rounded">restaurant_menu</span> Recipes Of The Day</a>
+          </div>
+        </nav>
+      </header>
+    </div>
+  </div>
 </body>
 </html>
