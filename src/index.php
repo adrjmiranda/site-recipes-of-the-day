a77bfc244bce678a79f4fe76979f8fb0a5b37c39
Adriano Miranda <adrjmiranda@gmail.com>
2023-06-26 20:56:54 -0300

    chore: add banner and carousel

--- src/index.php
+++ src/index.php
@@ -1,3 +1,22 @@
+<?php
+$categoriesBg = [
+  'rice_and_risotto.jpg',
+  'birds.jpg',
+  'meat.jpg',
+  'pastas.jpg',
+  'cakes.jpg',
+  'sweets_and_desserts.jpg',
+  'salads.jpg',
+  'snacks_and_snacks.jpg',
+  'starters_and_snacks.jpg',
+  'breads.jpg',
+  'fishes_and_sea_food.jpg',
+  'drinks.jpg',
+  'sauces_and_pâtés.jpg',
+  'soups_and_broths.jpg',
+  'special.jpg'
+];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +32,8 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Satisfy&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <!-- js scipts -->
+  <script src="js/scripts.js" defer></script>
 </head>
 <body>
   <div class="container-expanded">
@@ -22,7 +43,38 @@
           <div class="logo">
             <a href="#"><span class="material-symbols-rounded">restaurant_menu</span> Recipes Of The Day</a>
           </div>
+          <div id="menu">
+            <ul>
+              <li>
+                <a href="#">birds</a>
+              </li>
+              <li>
+                <a href="#">meat</a>
+              </li>
+              <li>
+                <a href="#">pastas</a>
+              </li>
+              <li>
+                <a href="#">cakes</a>
+              </li>
+              <li>
+                <a href="#">more <span class="material-symbols-rounded">arrow_drop_down</span></a>
+              </li>
+            </ul>
+          </div>
+          <div id="search">
+            <form action="">
+              <input type="search" name="search" id="search" placeholder="search recipe">
+              <button type="submit" class="btn"><span class="material-symbols-rounded">search</span></button>
+            </form>
+          </div>
+          <a href="#" class="btn login"><span class="material-symbols-rounded">person</span> enter</a>
         </nav>
+        <div id="banner">
+          <?php foreach($categoriesBg as $categoryBg): ?>
+            <div class="category-bg" style="background-image: url('assets/imgs/<?= $categoryBg ?>');"></div>
+          <?php endforeach; ?>
+        </div>
       </header>
     </div>
   </div>
---
+++ src/js/scripts.js
@@ -0,0 +1,47 @@
+let banner = document.querySelector('#banner');
+const categoryBackground = document.querySelectorAll('.category-bg');
+
+const timeStart = new Date().getTime();
+
+const animationTime = 2;
+const timeToStarAnimation = 5;
+const timeToRestartTheAnimation = 8;
+
+categoryBackground.forEach((item) => {
+	item.style.animationDuration = animationTime + 's';
+});
+
+const startAnimation = () => {
+	for (let index = 0; index < 4; index++) {
+		banner.children[index].classList.add('move-left-' + (index + 1));
+	}
+};
+
+const endAnimation = () => {
+	for (let index = 0; index < 4; index++) {
+		banner.children[index].classList.remove('move-left-' + (index + 1));
+	}
+};
+
+const resetAnimation = () => {
+	banner = document.querySelector('#banner');
+
+	let first = banner.children[0];
+
+	banner.removeChild(banner.children[0]);
+	banner.appendChild(first);
+};
+
+setInterval(async () => {
+	let time = new Date().getTime();
+	let interval = Math.round((time - timeStart) / 1000);
+
+	if (interval % timeToStarAnimation == 0) {
+		startAnimation();
+	}
+}, 1000);
+
+setInterval(() => {
+	endAnimation();
+	resetAnimation();
+}, timeToRestartTheAnimation * 1000);
