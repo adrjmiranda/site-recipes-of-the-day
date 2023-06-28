0911e4fd00e1517fae064c90e42311e8b36133af
Adriano Miranda <adrjmiranda@gmail.com>
2023-06-28 16:24:36 -0300

    chore: add style to footer.

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,32 +1,36 @@
 <footer id="footer">
   <div id="social-media">
-    <div class="social">
-      <div class="logo">
-        <a href="<?= $BASE_URL ?>index.php"><span class="material-symbols-rounded">restaurant_menu</span> Recipes Of
-          The Day</a>
+    <div class="container-wrapper">
+      <div class="social">
+        <div class="logo">
+          <a href="<?= $BASE_URL ?>index.php"><img src="<?= $BASE_URL ?>assets/recipesoftheday_logo.svg" alt="">
+            Recipes Of The Day</a>
+        </div>
+        <a href="#" class="social-btn btn">
+          <i class="bi bi-pinterest"></i>
+        </a>
+        <a href="#" class="social-btn btn">
+          <i class="bi bi-facebook"></i>
+        </a>
+        <a href="#" class="social-btn btn">
+          <i class="bi bi-instagram"></i>
+        </a>
+        <a href="#" class="social-btn btn">
+          <i class="bi bi-youtube"></i>
+        </a>
       </div>
-      <a href="#" class="social-btn">
-        <i class="bi bi-pinterest"></i>
-      </a>
-      <a href="#" class="social-btn">
-        <i class="bi bi-facebook"></i>
-      </a>
-      <a href="#" class="social-btn">
-        <i class="bi bi-instagram"></i>
-      </a>
-      <a href="#" class="social-btn">
-        <i class="bi bi-youtube"></i>
-      </a>
-    </div>
-    <div class="social-about">
-      <a href="#">who we are</a>
-      <a href="#">terms of use</a>
-      <a href="#">privacy</a>
-      <a href="#">contact</a>
+      <div class="social-about">
+        <a href="#">who we are</a>
+        <a href="#">terms of use</a>
+        <a href="#">privacy</a>
+        <a href="#">contact</a>
+      </div>
     </div>
   </div>
   <div id="reserved-rights">
-    Made with <i class="bi bi-heart-fill"></i> by Adriano Miranda &copy; 2023
+    <div class="container-wrapper">
+      Made with <i class="bi bi-heart-fill"></i> by <span>Adriano Miranda</span> &copy; 2023
+    </div>
   </div>
 </footer>
 </div>
