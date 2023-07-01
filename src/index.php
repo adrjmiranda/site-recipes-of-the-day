7adfde1d1f66361d5ef6e91e9b7cadc8ccc47cdd
Adriano Miranda <adrjmiranda@gmail.com>
2023-07-01 13:29:44 -0300

    chore: added special-card.

--- src/index.php
+++ src/index.php
@@ -116,50 +116,9 @@ $categoriesBg = [
 <div id="special">
   <div class="special container-wrapper">
     <h3 class="title">Special</h3>
-    <div class="special-card">
-      <div class="special-card-image"></div>
-      <div class="special-info">
-        <div class="special-title">
-          <h4>Lorem Ipsum is simply dummy</h4>
-        </div>
-        <div class="special-time">
-          <span><i class="bi bi-clock-fill"></i> 15min</span>
-        </div>
-      </div>
-    </div>
-    <div class="special-card">
-      <div class="special-card-image"></div>
-      <div class="special-info">
-        <div class="special-title">
-          <h4>Lorem Ipsum is simply dummy</h4>
-        </div>
-        <div class="special-time">
-          <span><i class="bi bi-clock-fill"></i> 15min</span>
-        </div>
-      </div>
-    </div>
-    <div class="special-card">
-      <div class="special-card-image"></div>
-      <div class="special-info">
-        <div class="special-title">
-          <h4>Lorem Ipsum is simply dummy</h4>
-        </div>
-        <div class="special-time">
-          <span><i class="bi bi-clock-fill"></i> 15min</span>
-        </div>
-      </div>
-    </div>
-    <div class="special-card">
-      <div class="special-card-image"></div>
-      <div class="special-info">
-        <div class="special-title">
-          <h4>Lorem Ipsum is simply dummy</h4>
-        </div>
-        <div class="special-time">
-          <span><i class="bi bi-clock-fill"></i> 15min</span>
-        </div>
-      </div>
-    </div>
+    <?php foreach ($specialRecipes as $specialRecipe): ?>
+      <?php require __DIR__ . '/templates/cards/special-card.php' ?>
+    <?php endforeach; ?>
   </div>
 </div>
 <div id="new-recipes">
--- src/templates/cards/special-card.php
+++ src/templates/cards/special-card.php
@@ -1,11 +1,17 @@
 <a href="#" class="special-card">
-  <div class="special-card-image"></div>
+  <div class="special-card-image"
+    style="background-image: url('<?= $specialRecipe->getRecipeImage() === '' ? $BASE_URL . 'assets/imgs/categories/' . str_replace(' ', '_', $specialRecipe->getCategory()) . '.jpg' : $BASE_URL . 'images/recipes/' . $specialRecipe->getId() . '/' . $specialRecipe->getRecipeImage() ?>');">
+    ></div>
   <div class="special-info">
     <div class="special-title">
-      <h4>Lorem Ipsum is simply dummy</h4>
+      <h4>
+        <?= $specialRecipe->getTitle() ?>
+      </h4>
     </div>
     <div class="special-time">
-      <span><i class="bi bi-clock-fill"></i> 15min</span>
+      <span><i class="bi bi-clock-fill"></i>
+        <?= $specialRecipe->getPreparationTime() ?>min
+      </span>
     </div>
   </div>
 </a>
